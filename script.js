8fe99c7fc8242d25387e001273b0438cba560922
orbitweb24 <orbitcomputeroffice@gmail.com>
2025-07-30 15:36:24 +0530

    practice download script.js

--- script.js
+++ script.js
@@ -170,7 +170,7 @@ document
   .getElementById("downloadButton")
   .addEventListener("click", function () {
     window.location.href =
-      "https://drive.google.com/uc?export=download&id=17rbiDBRFL5pvs7TAIJ0oj9btBNtfPjrC";
+      "https://drive.usercontent.google.com/download?id=17rbiDBRFL5pvs7TAIJ0oj9btBNtfPjrC&export=download&authuser=0";
     document.getElementById("downloadButton").textContent = "File Downloaded";
     document.getElementById("downloadButton").disabled = true;
   });
